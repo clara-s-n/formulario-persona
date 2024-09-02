ef37f2fb9ff2da947fd98b89342374358c102933
Nicolas Marquez <nicomars270@gmail.com>
2024-09-02 20:17:22 -0300

    Quedan cosas por hacer

--- frontend/editForm/main.js
+++ frontend/editForm/main.js
@@ -1,23 +1,40 @@
 const API_URL = 'http://localhost:3000';
 
-try {
-    const response = await fetch(`${API_URL}/personas/:id`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(persona),
-    });
 
-    if (response.ok) {
-        const persona = await response.json();
-        document.getElementById('nombre').value = persona.nombre;
-        document.getElementById('edad').value = persona.edad;
-        document.getElementById('email').value = persona.email;
-      } else {
-        console.error('Error al obtener los datos de la persona');
-      }
-} catch (error) {
-    console.error('Error al registrar la persona:', error);
-    alert('Error al registrar la persona');
+const cancelarBtn = document.getElementById('cancelarBtn');
+
+cancelarBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    window.location.href = '/';
+});
+
+async function obtenerDatosPersona(id) {
+    try {
+        const response = await fetch(`${API_URL}/personas/${id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+
+        if (response.ok) {
+            const persona = await response.json();
+            document.getElementById('nombre').value = persona.nombre;
+            document.getElementById('apellido').value = persona.apellido;
+            document.getElementById('email').value = persona.email;
+            document.getElementById('cedula').value = persona.cedula;
+            document.getElementById('rut').value = persona.rut;
+        } else {
+            console.error('Error al obtener los datos de la persona');
+        }
+    } catch (error) {
+        console.error('Error al obtener los datos de la persona:', error);
+        alert('Error al obtener los datos de la persona');
+    }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const pathParts = window.location.pathname.split('/');
+    const id = pathParts[pathParts.length - 2];
+    obtenerDatosPersona(id);
+});
