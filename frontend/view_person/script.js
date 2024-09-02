a00cfac430da568dabf8de444c8b22ff8717da9c
Nicolas Marquez <nicomars270@gmail.com>
2024-09-02 20:20:34 -0300

    Faltaba eto

--- frontend/view_person/script.js
+++ frontend/view_person/script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000personas';
+const API_URL = 'http://localhost:3000/personas';
 
 function getQueryParam(param) {
   const urlParams = new URLSearchParams(window.location.search);
