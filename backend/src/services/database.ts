5251fc1a670e90bbda6c53666da8373702d42d59
Nicolas Marquez <nicomars270@gmail.com>
2024-09-03 21:10:02 -0300

    Creacion service database y algunas cosas mas

---
+++ backend/src/services/database.ts
@@ -0,0 +1,16 @@
+import pg from 'pg'
+const { Pool } = pg
+ 
+const pool = new Pool({
+    'database': 'personas',
+    'user': 'admin',
+    'password': 'admin'
+})
+ 
+export const query = async (text: string, params?: (string|number|string[]|number[])[]) => {
+  // const start = Date.now()
+  const res = await pool.query(text, params)
+  // const duration = Date.now() - start
+  // console.log('executed query', { text, duration, rows: res.rowCount })
+  return res
+}
--- database/init.sql
+++ database/init.sql
@@ -1,8 +1,15 @@
-create table personas (
+create extension if not exists pgcrypto;
+
+
+create table if not exists personas (
     id SERIAL primary key,
     nombre text not null,
     apellido text not null,
     email text not null unique,
     cedula text not null unique,
-    rut bigint not null unique
-)
+    rut bigint not null unique,
+    password text not null
+);
+
+insert into personas (nombre, apellido, email, cedula, rut, password)
+    values('Martin', 'Perez', 'mperez@correo.com', '5.440.395-7', 213971680013, crypt('27DEenero2003_', gen_salt('bf')));
