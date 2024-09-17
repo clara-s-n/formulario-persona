55f6c77b6e0d425c1a4acf21375e08a8e570cdd1
Nicolas Marquez <nicomars270@gmail.com>
2024-09-17 14:20:17 -0300

    Redirect de google auth, falta que pase el token y los datos del usuario para el registro

--- backend/src/routes/auth/login/google/index.ts
+++ backend/src/routes/auth/login/google/index.ts
@@ -1,22 +1,38 @@
 import { FastifyPluginAsync } from 'fastify';
-
+import { query } from '../../../../services/database.js'
 const googleAuth: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
     // Endpoint de callback después del login exitoso
     fastify.get('/callback', {
         handler: async function (request, reply) {
             try {
 
-                const token = await fastify.googleOAuth2.getAccessTokenFromAuthorizationCodeFlow(request);
-    
-                //console.log(`access_token: ${token.token.access_token}`);
+                const googletoken = await fastify.googleOAuth2.getAccessTokenFromAuthorizationCodeFlow(request);
 
-                const userinfo = await fastify.googleOAuth2.userinfo(token.token.access_token)
+                const userinfo = await fastify.googleOAuth2.userinfo(googletoken.token.access_token);
 
-                reply.send({ 
-                    access_token: token.token.access_token,
-                    user_info: userinfo
-                 });
-                
+                // Para facilitar el llamado de las propiedades del objeto lo procesamos de json a string y viceversa.
+                const userinfoString = JSON.stringify(userinfo);
+
+                // Parsear la cadena JSON de nuevo a un objeto
+                const parsedUserinfo = JSON.parse(userinfoString);
+
+                // Acceder a las propiedades del usuario
+                const email = parsedUserinfo.email;
+                //const given_name = parsedUserinfo.given_name;
+                //const family_name = parsedUserinfo.family_name
+
+                // Consulta a la base de datos
+                const res = await query(`select id, email, password from personas where email = '${email}'`);
+
+                if (res.rows.length === 0) {
+                    reply.redirect('https://localhost/');
+                    return;
+                  }
+                //const user = res.rows[0];
+                //const token = fastify.jwt.sign({ id: user.id, email: user.email });
+                reply.redirect('https://localhost/peopleList')
+
+
             } catch (error) {
                 console.error('Error al obtener el token de acceso:', error);
                 reply.status(500).send({ error: 'Error al procesar la autenticación' });
--- database/init.sql
+++ database/init.sql
@@ -12,7 +12,7 @@ create table if not exists personas (
 );
 
 insert into personas (name, lastname, email, countryId, rut, password)
-    values('Martin', 'Perez', 'mperez@correo.com', '5.440.395-7', 213971680013, crypt('27DEenero2003_', gen_salt('bf'))),
+    values('Martin', 'Perez', 'nicomars270@gmail.com', '5.440.395-7', 213971680013, crypt('27DEenero2003_', gen_salt('bf'))),
           ('Lorena', 'Gonzalez', 'lorena@correo.com', '4.510.196-4', 130057830019, crypt('28DEenero2003_', gen_salt('bf'))),
           ('Celeste', 'Gomez', 'celeste@correo.com', '3.312.475-0', 150046270011, crypt('29DEenero2003_', gen_salt('bf'))),
           ('Juan', 'Perez', 'juan@correo.com', '5.483.597-4', 215081750014, crypt('30DEenero2003_', gen_salt('bf')));
